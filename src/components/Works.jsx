4dab43bb7b524b5e644eb60834313a5c063e04a1
DZykov <dzykov@protonmail.com>
2023-04-13 16:40:42 -0400

    add works mobile support

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { isMobile } from 'react-device-detect';
 import github from "../assets/skills/tech/github.png";
 import live from "../assets/general/live.png";
 
@@ -84,6 +84,35 @@ const ProjectCard = ({
 };
 
 const Works = () => {
+  if (isMobile) {
+    return (
+      <>
+        <div variants={textVariant()}>
+          <p className={`${styles.sectionSubText} `}>My work</p>
+          <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        </div>
+
+        <div className='w-full flex'>
+          <p
+            variants={fadeIn("", "", 0.1, 1)}
+            className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          >
+            Following projects showcases my skills and experience through
+            real-world examples of my work. Each project is briefly described with
+            links to code repositories and live demos in it. It reflects my
+            ability to solve complex problems, work with different technologies,
+            and manage projects effectively.
+          </p>
+        </div>
+
+        <div className='mt-20 flex flex-wrap gap-7'>
+          {projects.map((project, index) => (
+            <ProjectCard key={`project-${index}`} index={index} {...project} />
+          ))}
+        </div>
+      </>
+    )
+  }
   return (
     <>
       <motion.div variants={textVariant()}>
